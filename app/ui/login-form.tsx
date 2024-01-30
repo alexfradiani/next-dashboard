686eaf4131bf1705eca3357594f4a29c7236084d
Alex Frad <a.fradiani@gmail.com>
2024-01-30 21:06:22 +0100

    updating login page

--- app/ui/login-form.tsx
+++ app/ui/login-form.tsx
@@ -19,6 +19,10 @@ export default function LoginForm() {
       <div className="flex-1 rounded-lg bg-gray-50 px-6 pb-4 pt-8">
         <h1 className={`${lusitana.className} mb-3 text-2xl`}>
           Please log in to continue.
+          <br />
+          <span className="text-sm">
+            (use email: user@nextmail.com and password: 123456)
+          </span>
         </h1>
         <div className="w-full">
           <div>
